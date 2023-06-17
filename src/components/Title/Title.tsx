3c0844a5dfbc3c405cc342bdccaee8e7ccb1c99f
Eofre <yegorgordinskiy2002@gmail.com>
2023-06-18 01:19:27 +0800

    Modify component: Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,20 +1,31 @@
 import React, { CSSProperties, FC, ReactNode } from "react";
 import classes from "./Title.module.scss";
+import { Importance, Size, Theme } from "../../types/types";
 
 export interface ITitleProps {
   children: ReactNode;
-  importance: 1 | 2 | 3 | 4 | 5 | 6;
+  importance: Importance;
+  theme?: Theme;
+  size?: Size;
 }
 
-export const Title: FC<ITitleProps> = ({ children, importance }) => {
+export const Title: FC<ITitleProps> = ({
+  children,
+  importance,
+  theme = "dark",
+  size = "large",
+}) => {
+  const className: string = `${classes.title} ${classes[`title-${theme}`]} ${
+    classes[`title-${size}`]
+  }`;
   return (
     <>
-      {importance === 1 && <h1 className={classes.title}>{children}</h1>}
-      {importance === 2 && <h2 className={classes.title}>{children}</h2>}
-      {importance === 3 && <h3 className={classes.title}>{children}</h3>}
-      {importance === 4 && <h4 className={classes.title}>{children}</h4>}
-      {importance === 5 && <h5 className={classes.title}>{children}</h5>}
-      {importance === 6 && <h6 className={classes.title}>{children}</h6>}
+      {importance === 1 && <h1 className={className}>{children}</h1>}
+      {importance === 2 && <h2 className={className}>{children}</h2>}
+      {importance === 3 && <h3 className={className}>{children}</h3>}
+      {importance === 4 && <h4 className={className}>{children}</h4>}
+      {importance === 5 && <h5 className={className}>{children}</h5>}
+      {importance === 6 && <h6 className={className}>{children}</h6>}
     </>
   );
 };
